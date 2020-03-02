dc75ca178c9e6f291dfbb16e214774252fb7644e
Andrew Ng <ngandrew1337@gmail.com>
2020-03-02 12:35:25 -0900

    Adding vao and shaders

--- SnowSim/Main.cpp
+++ SnowSim/Main.cpp
@@ -14,6 +14,24 @@
 #include <random>
 
 
+const char* vertexShaderSource = 
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"uniform float yOffset;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y - yOffset, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+"}\n";
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
@@ -33,12 +51,12 @@ int main()
 	std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
 
 	std::vector<Particle> particles;
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 10; i++) {
 		Particle newParticle(dist(gen), dist(gen), dist(gen));
 		particles.push_back(newParticle);
 	}
 
-	std::vector<GLfloat> vertices;
+	std::vector<float> vertices;
 	for (auto p : particles) {
 		std::vector<float> pos = p.getPos();
 		/*std::cout << "x: " << pos[0];
@@ -79,22 +97,49 @@ int main()
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	GLuint buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices.front(), GL_STATIC_DRAW);
+	glPointSize(10.0f);
+
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices.front(), GL_DYNAMIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int program;
+	program = glCreateProgram();
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+
+	float offset = 0.0f;
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glMatrixMode(GL_MODELVIEW);
+		//glMatrixMode(GL_MODELVIEW);
 
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glPointSize(10.0f);
+		//glColor3f(1.0f, 1.0f, 1.0f);
+		//glPointSize(10.0f);
 		/*glBegin(GL_POINTS);
 
 		for (auto p : particles) {
@@ -104,10 +149,34 @@ int main()
 
 		glEnd();*/
 
-		glDrawArrays(GL_POINTS, 0, 100);
+		//std::cout << "\n\n\n";
+		for (auto v : vertices) {
+			v = v + 0.2f;
+			//std::cout << v << "\n";
+		}
+		offset += 0.001;
+		int location = glGetUniformLocation(program, "yOffset");
+		glUniform1f(location, offset);
+
+		glBindVertexArray(vao);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices.front(), GL_STREAM_DRAW);
+
+
+		glUseProgram(program);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_POINTS, 0, 10);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		glBindVertexArray(0);
+
+		//std::cout << "\n\n\n";
+		//for (auto v : vertices) {
+		//	std::cout << v << "\n";
+		//}
 	}
 
 	glfwTerminate();
